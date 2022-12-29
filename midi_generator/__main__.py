b9ef4b4a0187701ae829f222af1f848fd34e1015
Bonhun Koo <darkgs@snu.ac.kr>
2022-12-30 03:28:02 +0900

    Refine the project structure with consideration of packaging

---
+++ midi_generator/__main__.py
@@ -0,0 +1,9 @@
+"""
+Main function of midi_generator module.
+"""
+import sys
+
+from .train import main
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ midi_generator/train.py
@@ -0,0 +1,25 @@
+"""
+Train the MIDI Generator
+"""
+from pathlib import Path
+
+from torch.utils.data import DataLoader
+
+from midi_generator.dataset.midi import MIDIData, MIDIDataset
+
+
+def main():
+    """
+    Temporal test function
+    """
+    midi_files = [str(midi_path) for midi_path in Path("data/midis").rglob("*.mid")]
+    instruments = [MIDIData.INSTRUMENT.PIANO]
+    num_epoch = 1
+    cache_home = "cache/midis"
+
+    dataset = MIDIDataset(midi_files=midi_files, instruments=instruments, cache_home=cache_home)
+    train_dataloader = DataLoader(dataset)
+
+    for _ in range(num_epoch):
+        for _, data in enumerate(train_dataloader):
+            print(data.shape)
